0c53c127dafbc0511cb2bd48abcfa2e6df4a639b
Bruno Gomes <icestonebruno@gmail.com>
2021-10-27 19:43:10 -0300

    login front

---
+++ index.js
@@ -0,0 +1,30 @@
+const express = require('express');
+const routes = require('./src/routes/index');
+const mongoose = require('mongoose');
+require('dotenv').config()
+
+const server = express();
+
+// mongoose.connect(process.env.MONGO_CONNECTION_URL, { useUnifiedTopology: true, useNewUrlParser: true },
+//   (error) => {
+//     if (error) { console.log(error) }
+
+//     else { console.log('Mongo Connected') }
+
+//   })
+
+// mongoose.set('useFindAndModify', false);
+
+//escolhendo template engine
+server.set('view engine', 'ejs')
+
+//habilitador os arquivos static
+server.use(express.static("public"))
+
+//usar o req.body
+server.use(express.urlencoded({ extend: true }))
+
+//routes
+server.use(routes)
+
+server.listen(process.env.PORT, () => console.log('rodando'))
---
+++ src/routes/index.js
@@ -0,0 +1,15 @@
+const express = require('express');
+require('dotenv').config()
+
+const app = express();
+
+// create application/x-www-form-urlencoded parser
+app.use(express.json());
+app.use(express.urlencoded());
+
+
+app.get('/', (req, res) => {
+  return res.render('index.ejs')
+})
+
+module.exports = app
